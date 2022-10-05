a923d516c9781d79626faa4df70cb1724ab3914a
enesgunacti <gnctenes@gmail.com>
2022-10-05 22:17:51 +0300

    First Commit

---
+++ client/src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+
+    <div>
+      <h1>Register</h1>
+      <input type="email" name="email" placeholder="email" v-model="email" />
+      <br>
+      <input type="password" name="password" placeholder="password"  v-model="password"/>
+      <br>
+      <button @click="register">Register</button>
+    </div>
+
+</template>
+
+<script>
+import AuthanticationService from '../services/AuthanticationService'
+
+export default {
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  methods: {
+    async  register () {
+      await AuthanticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
